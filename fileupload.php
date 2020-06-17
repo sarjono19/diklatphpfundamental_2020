af17bc99c7e7c5f023cbac6be974ac653098fb59
sarjono <sarjono1@pln.co.id>
2020-06-17 11:47:37 +0800

    belajar hari terakhir

---
+++ fileupload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UPLOAD FILE</title>
+</head>
+<body>
+	<form action="namafile.php" method="post" enctype="multipart/form-data">
+		Pilih file;
+		<input type="file" name="berkas" />
+		<br>
+		<input type="submit" name="upload" value="upload" />
+	</form>
+</body>
+</html>
